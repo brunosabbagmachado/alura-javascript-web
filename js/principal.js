a873234b2b3720593d7bdbbd75f14fef75f52455
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-07-14 12:30:58 -0300

    concluindo modulo 2

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,37 @@
-var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
+//introdução do curso ao JavaScript
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Aparecida Nutricionista";
+
+
+//dados do primeiro paciente
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc');
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!"
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent =  "Altura inválida!"
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
+
+
