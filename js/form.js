9cdf0b263ba759f5d9f495fe742560d2f4a1926b
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-07-25 20:53:54 -0300

    finalizando modulo 6

--- js/form.js
+++ js/form.js
@@ -13,11 +13,10 @@ botaoAdicionar.addEventListener("click", function (event) {
     //cria a tr e td do paciente
     var pacienteTr = montaTr(paciente);
 
-    var erro = validaPaciente(paciente);
+    var erros = validaPaciente(paciente);
 
     if (erros.length > 0) {
-        var mensagemErro = document.querySelector("#mensagem-erro");
-        mensagemErro.textContent = erros;
+        exibeMensagensDeErro(erros);
         return;
     }
 
@@ -27,7 +26,20 @@ botaoAdicionar.addEventListener("click", function (event) {
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
 });
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+
+    erros.forEach(function (erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    })
+}
 
 function obtemPacienteDoFormulario(form) {
 
@@ -67,12 +79,28 @@ function validaPaciente(paciente) {
 
     var erros = [];
 
+    if(paciente.nome.length == 0){
+        erros.push("O nome não pode ser em branco");
+    }
+
     if (!validaPeso(paciente.peso)) {
         erros.push("Peso é inválido!");
     }
 
     if (!validaAltura(paciente.altura)) {
         erros.push("Altura é inválida!");
+    }
+
+    if(paciente.peso.length == 0){
+        erros.push("O peso não pode ser em branco");
+    }
+
+    if(paciente.altura.length == 0){
+        erros.push("A altura do paciente não pode ser em branco");
+    }
+
+    if(paciente.gordura.length == 0){
+        erros.push("A gordura do paciente não pode ser em branco");
     }
 
     return erros;
