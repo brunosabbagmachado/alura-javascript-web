12c363edf2442d473117ef92a45de1b593525a3f
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-07-27 12:00:59 -0300

    finalizando modulo 7

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,7 +1,31 @@
-var pacientes = document.querySelectorAll(".paciente");
+//VERSAO 1 - opção de codigo que funciona, porém nao apaga novas linhas criadas posteriormente
+    //var pacientes = document.querySelectorAll(".paciente");
+    //pacientes.forEach(function(paciente){
+    //    paciente.addEventListener("dblclick", function(){
+    //        this.remove();
+    //    });
+    //});
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        console.log("Fui clicado com duplo clique");
-    });
+//VERSÃO 2 - opção que funciona mas é mais robusta
+    //var tabela = document.querySelector("table");
+
+    //tabela.addEventListener("dblclick", function(event){
+    //     var alvoEvento = event.target;
+    //     var paiDoAlvo = alvoEvento.parentNode;
+    //     paiDoAlvo.classList.add("fadeOut");
+    //     setTimeout(function(){
+    //         paiDoAlvo.remove();
+    //     }, 500);   
+    //});
+
+//VERSÃO 3
+var tabela = document.querySelector("table");
+
+    tabela.addEventListener("dblclick", function(event){
+    event.target.parentNode.classList.add("fadeOut");
+
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
 });
+
